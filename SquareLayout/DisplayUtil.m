0c95732edfb32f3507e6d374b063a195251b9dbb
Jeff Byrnes <jnbyrnes@gmail.com>
2014-06-01 19:22:04 -0400

    fixed toolbar spacing on ios6 and ios7

--- SquareLayout/DisplayUtil.m
+++ SquareLayout/DisplayUtil.m
@@ -13,25 +13,23 @@
 + (CGRect)getRootFrame:(UIView *)mainWindow
 {
     CGRect rootFrame = [mainWindow bounds];
-    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
-        // adjust for status bar on ios7
-        // Check for orientation
-        UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
-        if (orientation == UIInterfaceOrientationPortrait) {
-            NSLog(@"#########UIInterfaceOrientationPortrait");
-            rootFrame.origin.y = rootFrame.origin.y + 20;
-            rootFrame.size.height = rootFrame.size.height - 20;
-        } else if (orientation == UIInterfaceOrientationLandscapeLeft) {
-            NSLog(@"#########UIInterfaceOrientationLandscapeLeft");
-            rootFrame.origin.x = rootFrame.origin.x + 20;
-            rootFrame.size.width = rootFrame.size.width - 20;
-        } else if (orientation == UIInterfaceOrientationLandscapeRight) {
-            NSLog(@"#########UIInterfaceOrientationLandscapeRight");
-            rootFrame.size.width = rootFrame.size.width - 20;
-        } else {
-            NSLog(@"#########UIInterfaceOrientationPortraitUpsideDown");
-            rootFrame.size.height = rootFrame.size.height - 20;
-        }
+    // adjust for status bar on ios7
+    // Check for orientation
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (orientation == UIInterfaceOrientationPortrait) {
+        NSLog(@"#########UIInterfaceOrientationPortrait");
+        rootFrame.origin.y = rootFrame.origin.y + 20;
+        rootFrame.size.height = rootFrame.size.height - 20;
+    } else if (orientation == UIInterfaceOrientationLandscapeLeft) {
+        NSLog(@"#########UIInterfaceOrientationLandscapeLeft");
+        rootFrame.origin.x = rootFrame.origin.x + 20;
+        rootFrame.size.width = rootFrame.size.width - 20;
+    } else if (orientation == UIInterfaceOrientationLandscapeRight) {
+        NSLog(@"#########UIInterfaceOrientationLandscapeRight");
+        rootFrame.size.width = rootFrame.size.width - 20;
+    } else {
+        NSLog(@"#########UIInterfaceOrientationPortraitUpsideDown");
+        rootFrame.size.height = rootFrame.size.height - 20;
     }
     return rootFrame;
 }
